200c78e6b33b22e9b5aff76e68c3852678eda076
Michael Kirk <michael.code@endoftheworl.de>
2021-01-07 10:45:18 -0800

    revert 11ebe69143f1efc0cddb7d14417424af54a1c31c which was upstreamed

--- geo/src/algorithm/euclidean_distance.rs
+++ geo/src/algorithm/euclidean_distance.rs
@@ -133,16 +133,6 @@ where
     }
 }
 
-impl<T> EuclideanDistance<T, Coordinate<T>> for Coordinate<T>
-where
-    T: Float,
-{
-    /// Minimum distance between two Coordinates
-    fn euclidean_distance(&self, other: &Coordinate<T>) -> T {
-        Line::new(*self, *other).euclidean_length()
-    }
-}
-
 impl<T> EuclideanDistance<T, MultiPoint<T>> for Point<T>
 where
     T: GeoFloat,
